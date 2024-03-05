36f4ea7a5c33127ed8a643b6d8a15ed7ee18e6e7
Dev-Shakil <shakilhossain1007@gmail.com>
2024-03-05 18:38:25 +0600

    category section done

---
+++ app/_services/GlobalApi.js
@@ -0,0 +1,31 @@
+const { gql, default: request } = require("graphql-request");
+
+const MASTER_URL = "https://api-ap-south-1.hygraph.com/v2/cltd34b8c3le807usuqw3dww0/master";
+
+const getCategory = async () => {
+    try {
+        const query = gql`
+            query Category {
+                categories {
+                    id
+                    name
+                    bgcolor {
+                        hex
+                    }
+                    icon {
+                        url
+                    }
+                }
+            }
+        `;
+
+        const result = await request(MASTER_URL, query);
+        return result;
+    } catch (error) {
+        console.error("Error fetching category:", error);
+        throw error; // Rethrow the error if needed
+    }
+};
+export default{
+    getCategory
+}
--- app/page.js
+++ app/page.js
@@ -1,11 +1,26 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Hero from "./_components/Hero";
+import CategoryList from "./_components/CategoryList";
+import GlobalApi from "./_services/GlobalApi";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [categoryList, setCategoryList] = useState([]);
+  useEffect(()=>{
+    getCategoryList();
+  },[]
+  )
+  const getCategoryList = ()=>{
+    GlobalApi.getCategory().then(resp=>{
+      setCategoryList(resp.categories)
+    })
+  }
   return (
     <>
       <Hero/>
+      <CategoryList categoryList={categoryList} />
     </>
   );
 }
