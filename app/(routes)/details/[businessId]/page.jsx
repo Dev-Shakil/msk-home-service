7e1060731c7293fadef922d56626dd99db3bf6ce
Dev-Shakil <shakilhossain1007@gmail.com>
2024-03-10 01:44:03 +0600

    Business Details Page done

--- app/(routes)/details/[businessId]/page.jsx
+++ app/(routes)/details/[businessId]/page.jsx
@@ -4,6 +4,9 @@ import { signIn, useSession } from 'next-auth/react'
 import React, { useEffect, useState } from 'react'
 import BusinessInfo from '../_components/BusinessInfo';
 import Image from 'next/image';
+import { Mail, MapPin } from 'lucide-react';
+import SuggestedBusinessList from '../_components/SuggestedBusinessList';
+import BusinessDescription from '../_components/BusinessDescription';
 
 const BusinessDetail = ({params}) => {
     const [businessList,setBusinessList] = useState({});
@@ -32,10 +35,17 @@ const BusinessDetail = ({params}) => {
         }
     }
     console.log(businessList)
-  return status=="authenticated" && businessList && (<div className="py-5">
-    {/* <BusinessInfo business={businessList}/> */}
-    <h3>{businessList?.name}</h3>
-    <Image src={businessList?.image?.[0].url} alt={businessList?.name} className="rounded-full h-[150px] object-cover" width={150} height={200} />
+  return status=="authenticated" && businessList && (<div className="py-8 md:py-15 px-10 md:px-20">
+    <BusinessInfo business={businessList}/>
+    <div className="grid grid-cols-3 mt-16">
+        <div className="col-span-3 md:col-span-2 order-last md:order-first">
+            <BusinessDescription business={businessList} />
+        </div>
+        <div className="w-full">
+            <SuggestedBusinessList business={businessList}/>
+        </div>
+    </div>
+   
   </div>
   )
 }
